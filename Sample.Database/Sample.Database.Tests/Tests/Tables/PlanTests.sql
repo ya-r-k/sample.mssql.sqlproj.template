d911ec526480aefadf7b58047933464756db1f2e
Yaraslau Smychkou <jaraslau.smyczkou@Gmail.com>
2023-08-10 11:24:27 +0300

    MSDT-2 Implemented tests base architecture

---
+++ Sample.Database/Sample.Database.Tests/Tests/Tables/PlanTests.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [sampleTests].[test_Constraint_PlanFrequency]
+AS
+BEGIN
+    -- Assert that the constraint exists
+    EXEC tSQLt.AssertObjectExists 'dbo.Plan.CK_Plan_Frequency';
+
+    -- Test valid Frequency values
+    EXEC tSQLt.AssertConstraintPasses 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = 0'; -- Once
+    EXEC tSQLt.AssertConstraintPasses 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = 1'; -- Daily
+    EXEC tSQLt.AssertConstraintPasses 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = 2'; -- Weekly
+    EXEC tSQLt.AssertConstraintPasses 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = 3'; -- Monthly
+    EXEC tSQLt.AssertConstraintPasses 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = 4'; -- Quarterly
+    EXEC tSQLt.AssertConstraintPasses 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = 5'; -- Yearly
+
+    -- Test invalid Frequency values
+    EXEC tSQLt.AssertConstraintFails 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = -1'; -- Invalid
+    EXEC tSQLt.AssertConstraintFails 'dbo.Plan', 'CK_Plan_Frequency', 'Frequency = 6';  -- Invalid
+END;
